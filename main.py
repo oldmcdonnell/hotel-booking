decfb7c5ba71fb42b451452ce09e28509345e4c7
oldmcdonnell <oldmcdonnell@gmail.com>
2024-02-21 17:24:11 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+df = pd.read_csv("hotels.csv")
+
+class Hotel:
+    def book(self):
+    pass
+
+class ReservationTicket:
+    def generate(self):
+    pass
+
+#main lookp
+print(df)
