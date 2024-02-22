88f17aff60ad962129649541eeccfe59eb604cc7
oldmcdonnell <oldmcdonnell@gmail.com>
2024-02-21 23:31:26 -0500

    added customer name and hotela name to ticket linked them to main

--- main.py
+++ main.py
@@ -7,10 +7,11 @@ df = pd.read_csv("hotels.csv", dtype={"id":str})
 class Hotel:
     def __init__(self, hotel_id):
         self.hotel_id = hotel_id
+        self.name = df.loc[df["id"] == self.hotel_id, "name"].squeeze()
     def book(self):
         """book a hotel by changing availability"""
         df.loc[df["id"] == self.hotel_id, "available"] = "no"
-        df.to_csv("hotel.csv", index=False)
+        df.to_csv("hotels.csv", index=False)
 
     def available(self):
         """check if hotel is available"""
@@ -22,19 +23,26 @@ class Hotel:
 
 class ReservationTicket:
     def __init__(self, customer_name, hotel_object):
-        pass
+        self.customer_name = customer_name
+        self.hotel = hotel_object
     def generate(self):
-        pass
+        content = f"""
+        Thank you for your reservation
+        Here are your booking details:
+        Name: {self.customer_name}
+        Hotel Name: {self.hotel.name}
+        """
+        return content
 
-#main lookp
+#main look
 print(df)
-hotel_id = input("Enter the id of the hotel: ")
-hotel = Hotel(hotel_id)
+hotel_ID = input("Enter the id of the hotel: ")
+hotel = Hotel(hotel_ID)
 
 if hotel.available():
     hotel.book()
     name = input("Enter your name: ")
-    reservation_ticket = ReservationTicket(name, hotel)
+    reservation_ticket = ReservationTicket(customer_name=name, hotel_object=hotel)
     print(reservation_ticket.generate())
 else:
     print("hotel is not free")
