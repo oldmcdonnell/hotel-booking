827fdc91b344f15bcd8bf55c9910852dedf014ba
oldmcdonnell <oldmcdonnell@gmail.com>
2024-02-21 21:27:08 -0500

    hotel updated

--- main.py
+++ main.py
@@ -1,14 +1,40 @@
 import pandas as pd
+import pyarrow
 
-df = pd.read_csv("hotels.csv")
+df = pd.read_csv("hotels.csv", dtype={"id":str})
+
 
 class Hotel:
+    def __init__(self, hotel_id):
+        self.hotel_id = hotel_id
     def book(self):
-    pass
+        """book a hotel by changing availability"""
+        df.loc[df["id"] == self.hotel_id, "available"] = "no"
+        df.to_csv("hotel.csv", index=False)
+
+    def available(self):
+        """check if hotel is available"""
+        availability = df.loc[df["id"] == self.hotel_id, "available"].squeeze()
+        if availability == "yes":
+            return True
+        else:
+            return False
 
 class ReservationTicket:
+    def __init__(self, customer_name, hotel_object):
+        pass
     def generate(self):
-    pass
+        pass
 
 #main lookp
 print(df)
+hotel_id = input("Enter the id of the hotel: ")
+hotel = Hotel(hotel_id)
+
+if hotel.available():
+    hotel.book()
+    name = input("Enter your name: ")
+    reservation_ticket = ReservationTicket(name, hotel)
+    print(reservation_ticket.generate())
+else:
+    print("hotel is not free")
