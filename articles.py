38769b6c433a4e94a41d12d7ee63931d19f5ec72
oldmcdonnell <oldmcdonnell@gmail.com>
2024-02-26 11:30:09 -0500

    added fpdf to articles

--- articles.py
+++ articles.py
@@ -1,7 +1,8 @@
-import pandas as pd
-import pyarrow
+from fpdf import FPDF
+import pandas
 
-df = pd.read_csv("articles.csv", dtype={"id":str})
+df = pandas.read_csv("articles.csv", dtype={"id": str})
+
 
 class Article:
     def __init__(self, article_id):
@@ -9,16 +10,36 @@ class Article:
         self.name = df.loc[df['id'] == self.id, 'name'].squeeze()
         self.price = df.loc[df['id'] == self.id, 'price'].squeeze()
 
+    def available(self):
+        in_stock = df.loc[df['id'] == self.id, 'in stock'].squeeze()
+        return in_stock
+
 
 class Receipt:
     def __init__(self, article):
         self.article = article
-        print(article)
 
     def generate(self):
-        pass
+        pdf = FPDF(orientation="P", unit="mm", format="A4")
+        pdf.add_page()
 
-#main look
+        pdf.set_font(family="Times", size=16, style="B")
+        pdf.cell(w=50, h=8, txt=f"Receipt nr.{self.article.id}", ln=1)
+
+        pdf.set_font(family="Times", size=16, style="B")
+        pdf.cell(w=50, h=8, txt=f"Article: {self.article.name}", ln=1)
+
+        pdf.set_font(family="Times", size=16, style="B")
+        pdf.cell(w=50, h=8, txt=f"Price: {self.article.price}", ln=1)
+
+        pdf.output("receipt.pdf")
+
+
 print(df)
-article_ID = input("Enter the id of the item to purchase : ")
-article = Article(article_ID)
+article_ID = input("Choose an article to buy: ")
+article = Article(article_id=article_ID)
+if article.available():
+    receipt = Receipt(article)
+    receipt.generate()
+else:
+    print("No such article in stock.")
